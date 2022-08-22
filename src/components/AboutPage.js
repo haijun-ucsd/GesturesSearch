768960b9b435c592db813f3ecdbe875892c511a0
NandiZhang <nzhangag@connect.ust.hk>
2022-08-22 22:58:59 +0800

    update about page vis

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,9 +1,83 @@
 import React, { useState , useEffect } from 'react';
 import './components.css';
-import Zoom from 'react-reveal/Zoom';
 import { Slide } from 'react-reveal';
+import { PieChart } from 'react-minimal-pie-chart';
+
+export const chartOptions = {
+    scaleShowGridLines : true,
+};
+  
+
+export const chartData = {
+labels: ["July", "August", "September", "October", "November", "December", "January"],
+datasets: [
+    {
+        label: "External Uploads",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [0, 20, 70, 10, 20, 50, 100]
+    },
+    {
+        label: "Internal Uploads",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: [100, 300, 250, 300, 520, 450, 500]
+    }
+]
+};
+
+var BarChartData = {
+	labels: ["Mall", "Library", "Street", "Hospital", "Park", "Fair"],
+	datasets: [
+		{
+            axis: 'y',
+			label: "My First dataset",
+			fillColor: "rgba(220,220,220,0.5)",
+			strokeColor: "rgba(220,220,220,0.8)",
+			highlightFill: "rgba(220,220,220,0.75)",
+			highlightStroke: "rgba(220,220,220,1)",
+			data: [365, 300, 240, 181, 156, 55]
+		}
+	]
+};
+
+var BarChartOptions = {
+    indexAxis: 'y',
+};
+
+var RoleData = {
+	labels: ["Parent", "Patient", "Staff"],
+	datasets: [
+		{
+			label: "My First dataset",
+			fillColor: "rgba(151,187,205,0.2)",
+			strokeColor: "rgba(151,187,205,0.2)",
+			highlightFill: "rgba(151,187,205,0.2)",
+			highlightStroke: "rgba(151,187,205,0.2)",
+			data: [12, 2, 6]
+		}
+	]
+};
 
 export default function AboutPage() {
+    var LineChart = require("react-chartjs").Line;
+    var VerticalBarChart = require("react-chartjs").Bar;
+    var RoleBar = require("react-chartjs").Bar;
+
+    // const SVG = () => {
+    //     return (
+    //         <svg width="960" height="960" id='svg'></svg>
+    //     )
+    // }
+
     return (
         <div className="AboutPageBox">
             {/* <div className='Home'>
@@ -12,6 +86,54 @@ export default function AboutPage() {
                     src="https://mb.cision.com/Public/14959/3307223/99660166e82b8824_800x800ar.png"
                 />
             </div> */}
+            <div className='Vis'>
+                <div className='Circles'>
+                {/* <svg width="960" height="960"></svg>
+                <script src="https://d3js.org/d3.v4.min.js"></script> */}
+                </div>
+                <PieChart className='Age'
+                    data={[
+                        { title: 'Baby', value: 1, color: '#e1f7d5' },
+                        { title: 'Child', value: 5, color: '#ffbdbd' },
+                        { title: 'Teen', value: 21, color: '#ffffff' },
+                        { title: 'Adult', value: 58, color: '#c9c9ff' },
+                        { title: 'Senior', value: 19, color: '#f1cbff' },
+                    ]}
+                    animate={true}
+                    label={({ dataEntry }) => dataEntry.title}
+                    labelStyle={(index) => ({
+                        fontSize: '5px',
+                        fontFamily: 'sans-serif',
+                      })}
+                    labelPosition={75}/>
+                <PieChart className='Pie'
+                    data={[
+                        { title: 'Standing', value: 10, color: '#01a7c2' },
+                        { title: 'Sitting', value: 15, color: '#007090' },
+                        { title: 'Walking', value: 20, color: '#8AE28D' },
+                        { title: 'Squatting', value: 5, color: '#eaebed' },
+                        { title: 'Pulling sth', value: 7, color: '#605e5e' },
+                    ]}
+                    animate={true}
+                    label={({ dataEntry }) => dataEntry.title}
+                    labelStyle={(index) => ({
+                        fontSize: '5px',
+                        fontFamily: 'sans-serif',
+                      })}
+                    labelPosition={75}
+                />
+            </div>
+            <div className='VisBar'>
+                <RoleBar className='RoleBar' data={RoleData} width="300" height="300"/>
+            </div>
+            <div className='HistoryTitle'>
+                <h5>Total Uploads</h5>
+                <h5>Most Common Sites in Uploads</h5>
+            </div>
+            <div className='UploadHistory'>
+                <LineChart className='LineChart' data={chartData} options={chartOptions} width="800" height="350"/>
+                <VerticalBarChart className='BarChart' data={BarChartData} options={BarChartOptions} width="400" height="350"/>
+            </div>
             <div className='Stats'>
                 <div className='Contributor'>
                     <svg viewBox="0 0 1152 1024" width="80" height="80" data-type="icon" class=""><path d="M768 770.612v-52.78c70.498-39.728 128-138.772 128-237.832 0-159.058 0-288-192-288s-192 128.942-192 288c0 99.060 57.502 198.104 128 237.832v52.78c-217.102 17.748-384 124.42-384 253.388h896c0-128.968-166.898-235.64-384-253.388z" data-node-id="dfc60944-e021-42cc-9b93-76fc034226d2" data-comp-instance-id="6f363700-94b8-4533-ac95-23ce0745c093" data-type="path" class=""></path><path d="M327.196 795.328c55.31-36.15 124.080-63.636 199.788-80.414-15.054-17.784-28.708-37.622-40.492-59.020-30.414-55.234-46.492-116.058-46.492-175.894 0-86.042 0-167.31 30.6-233.762 29.706-64.504 83.128-104.496 159.222-119.488-16.914-76.48-61.94-126.75-181.822-126.75-192 0-192 128.942-192 288 0 99.060 57.502 198.104 128 237.832v52.78c-217.102 17.748-384 124.42-384 253.388h279.006c14.518-12.91 30.596-25.172 48.19-36.672z" data-node-id="e1094c59-90c2-4c96-830c-0f4aa663f3b9" data-comp-instance-id="6f363700-94b8-4533-ac95-23ce0745c093" data-type="path" class=""></path></svg>
@@ -21,6 +143,9 @@ export default function AboutPage() {
                     <Slide bottom>
                     <h1>4</h1>
                     </Slide>
+                    <Slide bottom>
+                        <p>+2</p>
+                    </Slide>
                 </div>
                 <div className='Images'>
                     <svg viewBox="0 0 1024 1024" width="80" height="80" data-type="icon" class=""><path d="M598 512h234l-234-234v234zM640 214l256 256v426q0 34-26 60t-60 26h-470q-34 0-59-26t-25-60v-598q0-34 26-59t60-25h298zM682 42v86h-512v598h-84v-598q0-34 25-60t59-26h512z" data-node-id="d2763731-c468-4c49-b959-90b02dfaf833" data-comp-instance-id="b19b7319-9b69-4f01-a6f2-8690b80f6a89" data-type="path" class=""></path></svg>
@@ -28,7 +153,10 @@ export default function AboutPage() {
                     <h3 className="ImageTitle"><br></br>Images</h3><br></br>
                     </Slide>
                     <Slide bottom>
-                    <h1>2k+</h1>
+                    <h1>2,057</h1>
+                    </Slide>
+                    <Slide bottom>
+                        <p>+213</p>
                     </Slide>
                 </div>
                 <div className='Locations'>
@@ -38,6 +166,9 @@ export default function AboutPage() {
                     </Slide>
                     <Slide bottom>
                     <h1>42</h1>
+                    </Slide>
+                    <Slide bottom>
+                        <p>+5</p>
                     </Slide>
                 </div>
             </div>
@@ -90,8 +221,8 @@ export default function AboutPage() {
                 <h5>Sample Images</h5>
                 </Slide>
             </div>
-            <div class="row">
-                <div class="column">
+            <div className="row">
+                <div className="column">
                     <Slide bottom>
                     <img width="100%" src="https://www.eatthis.com/wp-content/uploads/sites/4/2022/07/woman-cooking.jpg?quality=82&strip=all"/>
                     </Slide>
@@ -105,7 +236,7 @@ export default function AboutPage() {
                     <img width="100%" src="https://www.sassyhongkong.com/wp-content/uploads/2021/09/camping-hong-kong-whats-on.png"/>
                     </Slide>
                 </div>
-                <div class="column">
+                <div className="column">
                     <Slide bottom>
                     <img width="100%" src="https://images.theconversation.com/files/223381/original/file-20180615-85822-1o2y44i.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip" />
                     </Slide>
@@ -119,7 +250,7 @@ export default function AboutPage() {
                     <img width="100%" src="https://domf5oio6qrcr.cloudfront.net/medialibrary/11537/4a78f148-d427-4209-8173-f33d04c44106.jpg"/>
                     </Slide>
                 </div>
-                <div class="column">
+                <div className="column">
                     <Slide bottom>
                     <img width="100%" src="https://domf5oio6qrcr.cloudfront.net/medialibrary/11537/4a78f148-d427-4209-8173-f33d04c44106.jpg"/>
                     </Slide>
@@ -133,7 +264,7 @@ export default function AboutPage() {
                     <img width="100%" src="https://cloudfront-us-east-2.images.arcpublishing.com/reuters/UY7DGYM4XVMB3IKMYIU7C7IM3U.jpg"/>
                     </Slide>
                 </div>
-                <div class="column">
+                <div className="column">
                     <Slide bottom>
                     <img width="100%" src="https://www.sassyhongkong.com/wp-content/uploads/2021/09/camping-hong-kong-whats-on.png"/>
                     </Slide>
